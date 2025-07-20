1e97168e31b17d2bb10b85e300be877ae886cfba
masoodomar <omarzaigham777@gmail.com>
2025-07-21 00:48:28 +0500

    Create and run the server

---
+++ cilent/tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./src/**/*.{html,js,jsx,ts,tsx}",
+    "./public/**/*.html",
+    "./*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
+
+// This configuration file sets up Tailwind CSS for a project, specifying the paths to the content files where Tailwind's utility classes will be applied.
+// It includes all HTML and JavaScript files in the `src` directory and the public HTML
