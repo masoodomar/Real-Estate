6d44494e39d8543138ced24301f613a681ebf60f
masoodomar <omarzaigham777@gmail.com>
2025-07-25 07:54:06 +0500

    new commits

--- cilent/tailwind.config.js
+++ cilent/tailwind.config.js
@@ -10,6 +10,3 @@ export default {
   },
   plugins: [],
 };
-
-// This configuration file sets up Tailwind CSS for a project, specifying the paths to the content files where Tailwind's utility classes will be applied.
-// It includes all HTML and JavaScript files in the `src` directory and the public HTML
